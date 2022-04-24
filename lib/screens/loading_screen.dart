04f5d6da32cf804a11dda8afd57a0d76a2871df5
TerrenceBeckham <terrencebeckham@icloud.com>
2022-04-24 13:53:36 +0200

    Added location permissions for both Android and ios

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
+// ignore: use_key_in_widget_constructors
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    debugPrint('position');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
+            getLocation();
             //Get the current location
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );
